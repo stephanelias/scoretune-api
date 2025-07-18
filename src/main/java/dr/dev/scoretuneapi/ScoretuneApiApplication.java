ea4f3a7802d6a1c107badcabe4b522ab40354c06
stephanelias <stephane.elias229@gmail.com>
2025-07-19 00:32:04 +0200

    Initialization

---
+++ src/main/java/dr/dev/scoretuneapi/ScoretuneApiApplication.java
@@ -0,0 +1,13 @@
+package dr.dev.scoretuneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoretuneApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoretuneApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dr/dev/scoretuneapi/ScoretuneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dr.dev.scoretuneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoretuneApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
