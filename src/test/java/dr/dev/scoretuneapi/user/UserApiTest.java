04d15d11ed6573f8a134864f2dd97f93c794b6cb
stephanelias <stephane.elias229@gmail.com>
2025-08-20 20:18:48 +0200

    [DRD-26] fix : fix typo on user api test file

--- src/test/java/dr/dev/scoretuneapi/user/UserApiTest.java
+++ src/test/java/dr/dev/scoretuneapi/user/UserApiTest.java
@@ -13,7 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class UseApiTest {
+public class UserApiTest {
 
     @Autowired
     private MockMvc mockMvc ;
